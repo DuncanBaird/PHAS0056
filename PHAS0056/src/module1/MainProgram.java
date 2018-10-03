dd6c8f43128624ffa537984d81132b4c38a8a5af
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-10-03 11:52:18 +0100

    Added assigny of values and printing to module1/MainProgram

--- PHAS0056/src/module1/MainProgram.java
+++ PHAS0056/src/module1/MainProgram.java
@@ -10,6 +10,21 @@ public class MainProgram {
 		Date myDate = new Date();
 		System.out.println(myDate);
 		System.out.println("Program finished");
+		
+		int x = 100; int y = 200;
+		// Print the value of x
+		// to the screen
+		System.out.println(x);
+		// Prefix the printing of the
+		// value with some descriptive
+		// information:
+		System.out.println("The value of x is "+x);
+		// Note the "" for any words.
+		// The + means append to the
+		// output going to the screen
+		System.out.println(" x = "+x+" and y = "+y);
+		// We can have any number of
+		// things appended using a '+'
 
 	}
 
