9dfb43974193e957bd556175f299ad45131dd4b1
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-10-07 15:01:01 +0100

    Added comments, still need to add conclusion for timer method when called

--- PHAS0056/src/module1/AlgortihmControl.java
+++ PHAS0056/src/module1/AlgortihmControl.java
@@ -2,28 +2,34 @@ package module1;
 
 public class AlgortihmControl {
 
+	// loop method for printing integers from 1 to 8
 	public static void loop() {
-		// loop for printing integers from 1 to 8
 		for(int i = 1; i <= 8; i++) {
 			System.out.println(i);
 			
 		}
 	}
+
+	// loop method for printing integers from 10 to -5
 	public static void decrement() {
-		// loop for printing integers from 10 to -5
+
 		for(int i = 10; i >= -5; i--) {
 			System.out.println(i);
 			
 		}
 
 	}
+
+	//loop method for printing 2.5 to 4.3 in 0.1 increments
 	public static void increment() {
-		// loop for printing 2.5 to 4.3 in 0.1 increments
 		for(double i = 25 ; i <= 43; i++) {
 			System.out.println(i/10);
 			
 		}
 	}
+
+	// loop method for running a chosen time interval and printing every chosen
+	// interval of loops the number of loops passed
 	public static int timer(double maxTime, int loopSteps) {
 		// timer method
 		// initialise variables
@@ -34,13 +40,14 @@ public class AlgortihmControl {
 		while(timeDif < (maxTime*1000)) {
 			timeDif = System.currentTimeMillis() - timeNow;
 			loops++;
-			if(loops % loopSteps == 0) { // if statement to print the number of loops every hundred loops
+			if(loops % loopSteps == 0) { // if statement to print the number of loops every set loopSteps
 				System.out.println("The number of Timer loops run so far is " + loops);
 			}	
 		}
-		return loops;
+		return loops; // method returns total number of loops done
 	}
-	
+
+	// main method
 	public static void main(String[] args) {
 		//// Calling methods from above
 		System.out.println("Result of loop method is");
@@ -54,7 +61,7 @@ public class AlgortihmControl {
 		System.out.println("Total loops run is " + loopsTotal);
 		int loopsTotal2 = timer(10,50000);
 		System.out.println("Total loops run is " + loopsTotal2);
-		System.out.println("Why result?");
+		System.out.println("Why result?"); //small explanation
 		
 	}
 
