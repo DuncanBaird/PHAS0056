c03ca0cd741e2e75ca2e42f1bdaf53031d2876ac
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-10-12 19:00:59 +0100

    Final commit for marking
    Proofread, adjusted commenting for marking and removed needless main
    class

--- PHAS0056/src/module2/Complex.java
+++ PHAS0056/src/module2/Complex.java
@@ -2,13 +2,11 @@ package module2;
 
 public class Complex {
 	
-	// member variables
-	double re; double im;
+	double re; double im; // real:re and imaginary:im components of a complex number
 	
-	// constructors
 	public Complex() {
-		this.re = 0;
-		this.im = 0;
+		this.re = 0.0;
+		this.im = 0.0;
 	}
 	
 	public Complex(double real, double imaginary) {
@@ -16,42 +14,42 @@ public class Complex {
 		this.im = imaginary;
 	}
 	
-	// method to return real value of complex number
 	public double real() {
 		return this.re;
 	}
 	
-	// method to return imaginary value of complex number
 	public double imag() {
 		return this.im;
 	}
 	
-	// method to return modulus of complex number
 	public double modulus() {
+		// |x + iy| = (x*x+y*y)^0.5
 		double mod = Math.sqrt(Math.pow(this.re, 2) + Math.pow(this.im, 2));
 		return mod;
 	}
 	
-	// method to return argument of complex number (in radians)
-	public double angle() {
+	public double angle() { // in radians
+		// argument(x+iy) = arctan(y/x)
 		double argument = Math.atan(this.im/this.re);
 		return argument;
 	}
 	
-	// method to return conjugate of complex number
 	public Complex conjugate() {
+		// [conjugate of x+iy] = x-iy
 		return new Complex(this.re,-1*this.im);
 	}
 	
-	// method to normalise complex number
 	public Complex normalised() {
+		// normalised z = z / |z|
 		double normalisation = 1/this.modulus();
 		return new Complex(normalisation*this.re,normalisation*this.im);
 	}
 	
 	// method for testing equality of 2 complex numbers
 	public boolean equals(Complex c) {
+		// initialising loop variable
 		boolean result = true;
+		// if statement to check equality of complex numbers
 		if ((this.re == c.re) && (this.im == c.im)) {
 			result = true;
 		} else {
@@ -61,8 +59,13 @@ public class Complex {
 		return result;
 	}
 	
+	public String toString() {	
+			return "Re(w) = "+this.re+", Im(w) = "+this.im;
+		}
+	
 	// method for defining Cartesian complex number from Polar form
 	public Complex setFromModulusAngle(double mag, double ang) {
+		// x = r*cos(theta) 	y = r*sin(theta)
 		this.re = mag * Math.cos(ang);
 		this.im = mag * Math.sin(ang);
 		return new Complex(this.re,this.im);
@@ -70,39 +73,30 @@ public class Complex {
 	
 	//// static methods
 	
-	// static method to add complex numbers
 	public static Complex add(Complex w, Complex z) {
+		// z + w = Re(z+w) + iIm(z+w)
 		return new Complex(w.re+z.re,w.im+z.im);
 	}
 	
-	// static method to subtract complex numbers
 	public static Complex subtract(Complex w, Complex z) {
+		// z - w = Re(z+w) - iIm(z+w)
 		return new Complex(w.re-z.re,w.im-z.im);
 	}
 	
-	// static method to multiply complex numbers
 	public static Complex multiply(Complex w, Complex z) {
+		// w * z = [Re(w)*(Re(z) - Im(w)*Im(z)]+i[Re(w)*Im(z)+Im(w)*Re(z)]
 		return new Complex(((w.re*z.re)-(w.im*z.im)),((w.re*z.im)+(w.im*z.re)));
 	}
 	
-	// static method to divide complex numbers
 	public static Complex divide(Complex w, Complex z) {
+		// w/z = [Re(w)*Re(z)+Im(w)*Im(z)/Re(z)^2+Im(z)]+i[Im(w)*Re(z)+Re(w)*Im(z)/Re(z)^2+Im(z)]
 		double quotient_real = ((w.re*z.re)+(w.im*z.im))/((Math.pow(z.re, 2)+Math.pow(z.im, 2)));
 		double quotient_im = ((w.im*z.re)-(w.re*z.im))/((Math.pow(z.re, 2)+Math.pow(z.im, 2)));
 		return new Complex(quotient_real, quotient_im);
 	}
 	
-	// method for converting to string complex number
-	public String toString() {	
-			return "Re(w) = "+this.re+", Im(w) = "+this.im;
-		}
+	// key variables for complex numbers
 	public static final Complex ONE = new Complex(1,0); 
 	public static final Complex ZERO = new Complex (0,0);
 	public static final Complex I = new Complex (0,1);
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
