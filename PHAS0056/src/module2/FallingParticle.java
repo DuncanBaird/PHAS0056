c91cfa106304cc96f8425021a11f8ae0932c5e51
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-10-12 19:09:25 +0100

    Final commit for marking
    Proofread, updated comments and adjusted formatting

--- PHAS0056/src/module2/FallingParticle.java
+++ PHAS0056/src/module2/FallingParticle.java
@@ -2,7 +2,7 @@ package module2;
 
 public class FallingParticle {
 
-	// member variables
+	// simulation variables
 	private double m; // mass of particle
 	private double d; // drag coefficient
 	private double t = 0; // time since drop 
@@ -11,7 +11,6 @@ public class FallingParticle {
 	private double v; // velocity of particle(measured upwards)
 	private static final double g = 9.80665 ; // acceleration due to gravity (PDG Physical Constants 2018)
 	
-	// constructors
 	public FallingParticle() {
 		m = 0.0;
 		d = 0.0;
@@ -22,43 +21,53 @@ public class FallingParticle {
 		this.d = d;
 	}
 	
-	// set and get methods
+	//// set and get methods
+	
+	// set initial height of particle
 	public void setH(double h) {
 		this.h = h;
 	}
+	
+	// return current position of particle
 	public double getZ() {
 		return this.z;
 	}
+	
+	// set velocity of particle
 	public void setV(double v) {
 		this.v = v;
 	}
+	
+	// return velocity of particle
 	public double getV() {
 		return this.v; 
 	}
+	
+	// return time elapsed since drop of particle
 	public double getT() {
 		return this.t;
 	}
 	
-	// other methods
+	//// other methods
+	
+	// progress simulation through a time step
 	public void doTimeStep(double deltaT) {
+		// compute acceleration
 		double a = (this.d*Math.pow(this.v, 2)/this.m)-this.g;
+		// update particle velocity
 		this.v = this.v + a*deltaT;
+		// update particle position
 		this.z = this.z + this.v*deltaT;
 	}
 	
+	// start drop of particle in simulation with time increment deltaT
 	public void drop(double deltaT) {
-		this.z = this.h;
-		this.t = 0;
+		this.z = this.h; // initialise particle position
+		this.t = 0; // initialise time frame
+		// loop to run simulation till particle reaches bottom of vessel
 		while(this.z>0) {
-			this.t += deltaT;
+			this.t += deltaT; // increment time by time increment
 			doTimeStep(deltaT);
-			
 		}
 	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
