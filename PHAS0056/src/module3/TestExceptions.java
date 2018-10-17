e50a4b2e86fa21d12fd2550cdd26e02ded0bc574
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-10-17 16:25:24 +0100

    Completed testing in TestExceptions
    of methods that should throw exceptions in try and catch blocks

--- PHAS0056/src/module3/TestExceptions.java
+++ PHAS0056/src/module3/TestExceptions.java
@@ -5,6 +5,65 @@ public class TestExceptions {
 	public static void main(String[] args) {
 		// Testing Complex, ThreeVector and FallingParticle classes
 		
+		//// Complex
+		Complex w = new Complex(0,0);
+		Complex z = new Complex(1,0);
+		
+		System.out.println("Complex number w is " +w);
+		System.out.println("Complex number z is " +z);
+		
+		try { // attempt z/0 type calculation
+			System.out.println(Complex.divide(z, w));
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		try { // attempt to normalise 0
+			System.out.println(w.normalised());
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}		
+		//// ThreeVector
+		ThreeVector v1 = new ThreeVector(0,0,0);
+		ThreeVector v2 = new ThreeVector(2,2,2);
+		
+		System.out.println("Vector v1 is " + v1);
+		System.out.println("Vector v2 is " + v2);
+		
+		try { // attempt to find unitvector
+			System.out.println(v1.unitVector());
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		try { // attempt to find angle
+			System.out.println(ThreeVector.angle(v1, v2));
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		//// FallingParticle
+		/*FallingParticle particle = null;*/
+		try { // attempt to drop negative mass
+			new FallingParticle(-5,2);
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		try { // attempt to drop with negative drag
+			new FallingParticle(5,-2);
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+		try { // attempt to drop from below bottom
+			FallingParticle particle = new FallingParticle(10,5);
+			particle.setH(-10);
+			}
+		catch (Exception e) {
+			System.out.println(e);
+		}
 	}
-
 }
