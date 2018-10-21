2743218fc67054fe86873c0a4b3772c6d43e6e09
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-10-21 14:41:03 +0100

    Added comments, adjusted spacing

--- PHAS0056/src/module3/Alphabet.java
+++ PHAS0056/src/module3/Alphabet.java
@@ -4,11 +4,10 @@ import java.util.Random;
 import java.lang.Character;
 import java.lang.Integer;
 import java.lang.StringBuilder;
-import java.lang.Math;
-
 
 public class Alphabet {
 
+	// generates random character in integer range 0-127 (inclusive)
 	public static char randomCharacter() {
 		Random random = new Random();
 		int integer = random.nextInt(127)+1;
@@ -17,15 +16,18 @@ public class Alphabet {
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Using randomChracter method to build string given specific filters for characters
 		
-		StringBuilder characters = new StringBuilder(250);
+		StringBuilder characters = new StringBuilder(260); // create stringbuilder object
+		// initialise loop variables
 		int total = 0;
 		int exceptions = 0;
 		
+		// loop for repeating 400 times
 		for (int i=1; i<=400;i++) {
 			char character = randomCharacter();
 			
+			// check if char is letter or digit
 			if(Character.isLetterOrDigit(character)) {
 				characters.append(character);
 				try {
@@ -34,8 +36,7 @@ public class Alphabet {
 				catch (Exception e) {
 					exceptions ++;
 				}
-			}
-			
+			}		
 		}
 		System.out.println("The contents of the stringbuilder is: " + characters);
 		System.out.println("The number of characters in the StringBuilder is:" + characters.length());
