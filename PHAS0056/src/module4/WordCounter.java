1666105e6c2a19c2e711079ef83988070f7d41f4
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-10-22 15:16:38 +0100

    Wrote brFromURL method

--- PHAS0056/src/module4/WordCounter.java
+++ PHAS0056/src/module4/WordCounter.java
@@ -1,7 +1,18 @@
 package module4;
 
+import java.io.*;
+import java.net.URL;
+
 public class WordCounter {
 
+	public static BufferedReader brFromURL(String urlName) throws IOException {
+		URL u = new URL(urlName);
+		InputStream is = u.openStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		return br;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
