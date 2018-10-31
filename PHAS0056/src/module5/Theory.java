3991cf763b7a970b094b4d39b8f86ff3ca73436b
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-10-31 11:12:46 +0000

    Wrote class Theory with constructor, method to set member variable and function

--- PHAS0056/src/module5/Theory.java
+++ PHAS0056/src/module5/Theory.java
@@ -2,4 +2,16 @@ package module5;
 
 public class Theory {
 
+	private int n;
+	
+	//set n
+	public Theory(int n) {
+		this.n = n;
+	}
+	
+	//calculates function y=x^n
+	public double y(double x) {
+		return Math.pow(x, this.n);
+	}
+
 }
