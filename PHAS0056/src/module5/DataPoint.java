0d13ce372636222f76524bb04787f147387453d5
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-10-31 11:01:07 +0000

    Wrote DataPoint with getter and set methods for member variables

--- PHAS0056/src/module5/DataPoint.java
+++ PHAS0056/src/module5/DataPoint.java
@@ -2,9 +2,38 @@ package module5;
 
 public class DataPoint {
 	
+	//sets member variables for data measurements
 	private double x;
 	private double y;
 	private double ey;
 	
 	
+	//sets values to zero for default
+	public DataPoint() {
+		this.x =0.0d;
+		this.y = 0.0d;
+		this.ey = 0.0d;
+		
+	}
+	
+	//sets values
+	public DataPoint(double x, double y, double ey) {
+		this.x = x;
+		this.y = y;
+		this.ey = ey;
+	}
+	
+	////getter methods for each member variable
+	
+	public double getx() {
+		return this.x;
+	}
+	
+	public double gety() {
+		return this.y;
+	}
+	
+	public double getey() {
+		return this.ey;
+	}
 }
