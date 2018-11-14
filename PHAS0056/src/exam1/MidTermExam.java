539cef0a42346a75f9d6829b8b5d2e68b7b5a7a5
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-11-14 10:26:15 +0000

    Created ArrayList for playa data

--- PHAS0056/src/exam1/MidTermExam.java
+++ PHAS0056/src/exam1/MidTermExam.java
@@ -1,9 +1,14 @@
 package exam1;
 
+import java.util.ArrayList;
+
 public class MidTermExam {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Analysis of NFL player data
+
+		// create ArrayList collection for storying player data
+		ArrayList<Player> players = new ArrayList<Player>();
 
 	}
 
