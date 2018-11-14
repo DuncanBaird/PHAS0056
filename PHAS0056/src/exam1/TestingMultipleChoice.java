b111d55aee6df769f40925c63bc27753cb0ae13f
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-11-14 09:59:08 +0000

    Finished multiple choice

--- PHAS0056/src/exam1/TestingMultipleChoice.java
+++ PHAS0056/src/exam1/TestingMultipleChoice.java
@@ -4,8 +4,16 @@ public class TestingMultipleChoice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int a = 0; a < 10; ++a)
+		for (int a = 0; a < 2; ++a)
 			System.out.println("hello");
+		double x = 2.6d;
+		System.out.println((int) (x + 0.5));
+
+		int a = 0;
+		a += 1;
+		a += 4;
+		a += 8;
+		System.out.println(a);
 	}
 
 }
