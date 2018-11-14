2497e40af44eab3d62d141f1370a83ffb79b0664
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-11-14 12:39:58 +0000

    Adjusted commenting

--- PHAS0056/src/exam1/Player.java
+++ PHAS0056/src/exam1/Player.java
@@ -57,98 +57,57 @@ public class Player {
 		this.fumbles = fumbles;
 	}
 
-	//// get methods
+	//// get methods for each member variable
 
-	/**
-	 * @return the playerName
-	 */
 	public String getPlayerName() {
 		return playerName;
 	}
 
-	/**
-	 * @return the team
-	 */
 	public String getTeam() {
 		return team;
 	}
 
-	/**
-	 * @return the playerPosition
-	 */
 	public String getPlayerPosition() {
 		return playerPosition;
 	}
 
-	/**
-	 * @return the numberGames
-	 */
 	public int getNumberGames() {
 		return numberGames;
 	}
 
-	/**
-	 * @return the rushAttempts
-	 */
 	public int getRushAttempts() {
 		return rushAttempts;
 	}
 
-	/**
-	 * @return the rushYards
-	 */
 	public int getRushYards() {
 		return rushYards;
 	}
 
-	/**
-	 * @return the rushTouchDowns
-	 */
 	public int getRushTouchDowns() {
 		return rushTouchDowns;
 	}
 
-	/**
-	 * @return the target
-	 */
 	public int getTarget() {
 		return target;
 	}
 
-	/**
-	 * @return the receptions
-	 */
 	public int getReceptions() {
 		return receptions;
 	}
 
-	/**
-	 * @return the receivingYards
-	 */
 	public int getReceivingYards() {
 		return receivingYards;
 	}
 
-	/**
-	 * @return the receivingTouchDowns
-	 */
 	public int getReceivingTouchDowns() {
 		return receivingTouchDowns;
 	}
 
-	/**
-	 * @return the fumbles
-	 */
 	public int getFumbles() {
 		return fumbles;
 	}
 
 	// For printing player details
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
 	@Override
 	public String toString() {
 		return "[Player Name=" + playerName + ", Team=" + team + ", Position=" + playerPosition + ", Number of games="
@@ -163,7 +122,7 @@ public class Player {
 	public static Player parseLine(String line) {
 		Player p = new Player();
 		Scanner s = new Scanner(line);
-		s.useDelimiter("\t");
+		s.useDelimiter("\t");// tab as delimiter
 
 		while (s.hasNext()) {
 			p.playerName = s.next();
@@ -185,10 +144,7 @@ public class Player {
 		return p;
 	}
 
-	// get data from URL
-	// reads URL and returns data as ArrarList
-
-	// create array list from URL inputed data
+	// reads URL and returns data as ArraryList
 	public static ArrayList<Player> dataFromURL(String urlName) throws IOException {
 		URL u = new URL(urlName);
 		InputStream is = u.openStream();
