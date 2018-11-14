f84495cd3c95a85f1631e550eca974eae59bf7bb
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-11-14 12:29:40 +0000

    Completed core functionality and doing comments

--- PHAS0056/src/exam1/MidTermExam.java
+++ PHAS0056/src/exam1/MidTermExam.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 public class MidTermExam {
 
 	public static void main(String[] args) {
-		// Analysis of NFL player data
+		//// Analysis of NFL player data
 
 		// create ArrayList collection for storying player data
 		ArrayList<Player> players = new ArrayList<Player>();
@@ -16,9 +16,9 @@ public class MidTermExam {
 			// import data and assign to ArrayList from URL
 			players = Player.dataFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/exam-data/NFL2012OffensiveStats.txt");
 
-			// create hashmap of players ArrayList
+			// create HashMap of players ArrayList
 			HashMap<String, ArrayList<Player>> teams = new HashMap<String, ArrayList<Player>>();
-			// create arraylist of teams
+			// create ArrayList of teams
 			for (Player player : players) {
 				String currentTeam = player.getTeam();
 				ArrayList<Player> currentPlayers = teams.get(currentTeam);
@@ -48,8 +48,6 @@ public class MidTermExam {
 				}
 			}
 
-
-			System.out.println(players.get(2));
 			System.out.println("The total number of players recorded in the file is: " + players.size());
 			
 			System.out.println("The player with the maximum number of rushing yards "
@@ -58,33 +56,26 @@ public class MidTermExam {
 			System.out.println("The player with the maximum number of all purpose yards " + "is:"
 					+ players.get(playerMaxAllPurposeYardsUUID));
 			
-			//// For each team section
+			//// For each team
 
 			// Players with more than 10 touches
 			HashMap<String, ArrayList<Player>> tenPlusTouches = new HashMap<String, ArrayList<Player>>();
 			
-			// finds players with >10 AB
-			for (String team : teams.keySet()) {
-				for (Player player : teams.get(team)) {
+			for (String team : teams.keySet()) { // loops over teams
+				for (Player player : teams.get(team)) { // loops over players in team
 					ArrayList<Player> currentPlayers = tenPlusTouches.get(team);
 					if(currentPlayers == null) {
 						tenPlusTouches.put(team, new ArrayList<Player>());
 					}
 					if (Player.touches(player) >= 10) {
+						// add player details if touches are sufficient
 						tenPlusTouches.get(team).add(player);
 					}
 				}
-			}
-			
-			for (String team : tenPlusTouches.keySet()) {
 				System.out.println("The number of players with more than ten touches for " + team + " is: "
 						+ tenPlusTouches.get(team).size());
-				/*
-				 * System.out.println("The player with the highest liability for " + team +
-				 * " is: " + players.get(playerMaxLiabilityUUID));
-				 */
 			}
-			
+
 			// greatest liability
 			
 			HashMap<String, ArrayList<Player>> largestLiability = new HashMap<String, ArrayList<Player>>();
@@ -92,13 +83,13 @@ public class MidTermExam {
 			for (String team : teams.keySet()) {
 				for (Player player : teams.get(team)) {
 					ArrayList<Player> currentPlayers = largestLiability.get(team);
-					// System.out.println(player.getPlayerName());
 					if (currentPlayers == null) {
 						largestLiability.put(team, new ArrayList<Player>());
 					}
 					largestLiability.get(team).add(player);
 					if (maxLiability < Player.liability(player)) {
-							maxLiability = Player.liability(player);
+						maxLiability = Player.liability(player);
+						// if liability is higher update ArrayList
 						largestLiability.get(team).set(0, player);
 						}
 					}
@@ -112,13 +103,13 @@ public class MidTermExam {
 			for (String team : tenPlusTouches.keySet()) {
 				for (Player player : tenPlusTouches.get(team)) {
 					ArrayList<Player> currentPlayers = largestButter.get(team);
-					// System.out.println(player.getPlayerName());
 					if (currentPlayers == null) {
 						largestButter.put(team, new ArrayList<Player>());
 					}
 					largestButter.get(team).add(player);
 					if (maxButter < Player.butterFingers(player)) {
 						maxButter = Player.butterFingers(player);
+						// if butter fingers is higher update ArrayList
 						largestButter.get(team).set(0, player);
 					}
 				}
@@ -127,6 +118,9 @@ public class MidTermExam {
 			}
 
 		} catch (Exception e) {
+			// if exception is thrown catch
+			// analysis contained in 1 try as pointless to continue
+			// if there is an exception
 			System.out.println(e);
 		}
 
