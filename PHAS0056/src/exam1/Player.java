ef655e26c14e9511c4160a103c40a27daa2b8878
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-11-14 09:37:54 +0000

    Created package exam1
    Wrote member variables

---
+++ PHAS0056/src/exam1/Player.java
@@ -0,0 +1,19 @@
+package exam1;
+
+public class Player {
+
+	// member variables
+	private String playerName;
+	private String team;
+	private String playerPosition;
+	private int numberGames;
+	private int rushAttempts;
+	private int rushYards;
+	private int rushTouchDowns;
+	private int target;
+	private int receptions;
+	private int receivingYards;
+	private int receivingTouchDowns;
+	private double fumbles;
+
+}
