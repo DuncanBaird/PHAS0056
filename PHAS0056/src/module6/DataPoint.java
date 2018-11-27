bbacd81964769c541fe16e7037918bbdeb8550b3
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-11-27 09:21:04 +0000

    Created module6 and copied DataPoint from module5

---
+++ PHAS0056/src/module6/DataPoint.java
@@ -0,0 +1,64 @@
+package module6;
+
+import java.util.Scanner;
+
+public class DataPoint {
+	
+	//sets member variables for data measurements 
+	private double x;
+	private double y;
+	private double ey;
+	
+	
+	//sets values to zero for default
+	public DataPoint() {
+		this.x =0.0d;
+		this.y = 0.0d;
+		this.ey = 0.0d;
+		
+	}
+	
+	//sets values
+	public DataPoint(double x, double y, double ey) {
+		this.x = x;
+		this.y = y;
+		this.ey = ey;
+	}
+	
+	////getter methods for each member variable
+	
+	public double getx() {
+		return this.x;
+	}
+	
+	public double gety() {
+		return this.y;
+	}
+	
+	public double getey() {
+		return this.ey;
+	}
+	
+	//toString for DataPoint object
+	public String toString() {
+		return "x = " + x + ", y = " + y + ", ey = " + ey + "\n";
+	}
+	
+	
+	//Parser for input string
+	public static DataPoint parseLine(String line) {
+		Scanner s = new Scanner(line);
+		double x = 0.0d;
+		double y = 0.0d;
+		double ey = 0.0d;
+		//iterate over input tokens
+		while(s.hasNext()) {
+			//update values
+			x = s.nextDouble();
+			y = s.nextDouble();
+			ey = s.nextDouble();
+		}
+		s.close();
+		return new DataPoint(x,y,ey);
+	}
+}
