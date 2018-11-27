a27697e1776c7d589fb4629b38f0820b489c74f4
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-11-27 12:54:47 +0000

    Created TestDataPoints class

---
+++ PHAS0056/src/module6/TestDataPoints.java
@@ -0,0 +1,14 @@
+package module6;
+
+public class TestDataPoints {
+
+	public static dataFromURL(String URL) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
