5afe6d27de40dc60e0499643b1d7a2a44d3ffdad
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-11-27 12:52:34 +0000

    Added parser to LabelledDataPoint

--- PHAS0056/src/module6/LabelledDataPoint.java
+++ PHAS0056/src/module6/LabelledDataPoint.java
@@ -1,5 +1,7 @@
 package module6;
 
+import java.util.Scanner;
+
 public class LabelledDataPoint extends DataPoint {
 
 	String labelDataPoint;
@@ -18,6 +20,33 @@ public class LabelledDataPoint extends DataPoint {
 	@Override
 	public String toString() {
 		return "LabelledDataPoint [labelDataPoint=" + labelDataPoint + ", toString()=" + super.toString() + "]";
+	}
+
+	// Parser for input string
+	public static DataPoint parseLine(String line) {
+		Scanner s = new Scanner(line);
+		double x = 0.0d;
+		double y = 0.0d;
+		double ey = 0.0d;
+		String label = "";
+		// iterate over input tokens
+		while (s.hasNext()) {
+			// update values
+			x = s.nextDouble();
+			y = s.nextDouble();
+			ey = s.nextDouble();
+			if (s.hasNext()) {
+				label = s.next();
+			}
+		}
+		s.close();
+
+		if (label != "") {
+			return new LabelledDataPoint(x, y, ey, label);
+		} else {
+
+			return new DataPoint(x, y, ey);
+		}
 	}
 
 }
