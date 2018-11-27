2c3a3a061ba0cdf72004e9bc2068becc7469868f
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-11-27 13:05:49 +0000

    Added dataFromURL method to TestDataPoints

--- PHAS0056/src/module6/TestDataPoints.java
+++ PHAS0056/src/module6/TestDataPoints.java
@@ -1,9 +1,32 @@
 package module6;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+
 public class TestDataPoints {
 
-	public static dataFromURL(String URL) {
-		
+	// create array list from URL inputed data
+	public static ArrayList<DataPoint> dataFromURL(String urlName) throws IOException {
+		URL u = new URL(urlName);
+		InputStream is = u.openStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		String line = "";
+
+		// create array list
+		ArrayList<DataPoint> array1 = new ArrayList<DataPoint>();
+
+		// adds tokens from URL to array list
+		while ((line = br.readLine()) != null) {
+			DataPoint dp = DataPoint.parseLine(line);
+			array1.add(dp);
+		}
+		return array1;
 	}
 
 	public static void main(String[] args) {
