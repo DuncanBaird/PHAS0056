027c74f10b23f43b14ea88b296bdff180095c24c
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-11-27 14:45:53 +0000

    Wrote class QuadraticTheory

---
+++ PHAS0056/src/module6/QuadraticTheory.java
@@ -0,0 +1,23 @@
+package module6;
+
+public class QuadraticTheory {
+
+	private double a;
+	private double b;
+	private double c;
+
+	public QuadraticTheory(double a, double b, double c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	public double y(double x) {
+		return (a * Math.pow(x, 2) + b * x + c);
+	}
+
+	@Override
+	public String toString() {
+		return a + "x^2 + " + b + "x + " + c;
+	}
+}
