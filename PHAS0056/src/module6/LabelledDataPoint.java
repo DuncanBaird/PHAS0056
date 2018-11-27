e8e9594144df57648e6581de63f37deda10364f5
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-11-27 09:31:15 +0000

    Made subclass LabelledDataPoint with labelDataPoint member variable

---
+++ PHAS0056/src/module6/LabelledDataPoint.java
@@ -0,0 +1,11 @@
+package module6;
+
+public class LabelledDataPoint extends DataPoint {
+
+	String labelDataPoint;
+
+	public LabelledDataPoint() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
