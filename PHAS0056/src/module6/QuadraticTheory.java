25383a6615083e88057ff8df8805413e535b89e5
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-12-02 17:00:15 +0000

    Added comments

--- PHAS0056/src/module6/QuadraticTheory.java
+++ PHAS0056/src/module6/QuadraticTheory.java
@@ -12,10 +12,12 @@ public class QuadraticTheory implements Theory {
 		this.c = c;
 	}
 
+	// quadratic function
 	public double y(double x) {
 		return (a * Math.pow(x, 2) + b * x + c);
 	}
 
+	// to string for quadratic object
 	@Override
 	public String toString() {
 		return a + "x^2 + " + b + "x + " + c;
