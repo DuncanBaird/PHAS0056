58fd32e58d3f39c5929955a5fdbd3ef3ed68c496
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-12-03 15:23:53 +0000

    PrimeNumberTask wrote isPrime checker method

--- PHAS0056/src/module8/PrimeNumberTask.java
+++ PHAS0056/src/module8/PrimeNumberTask.java
@@ -1,5 +1,29 @@
 package module8;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class PrimeNumberTask implements Runnable {
 
+	private static Collection<Integer> primeList = new ArrayList<Integer>();
+	private static int largestN;
+	private static int largestPrime;
+
+	// test if input is prime
+	public static boolean isPrime(int n) {
+		if (n < 2) {
+			return false;
+		} else {
+			for (int i = 2; i <= Math.sqrt(n); i++) {
+				if (n % i == 0) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	public void run() {
+
+	}
 }
