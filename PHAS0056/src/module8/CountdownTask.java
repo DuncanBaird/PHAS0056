1f1ab69681cfd7799035675e65dca6ef1f62fdc5
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-12-03 14:52:23 +0000

    Added Thread.sleep to CountdownTask

--- PHAS0056/src/module8/CountdownTask.java
+++ PHAS0056/src/module8/CountdownTask.java
@@ -9,8 +9,13 @@ public class CountdownTask implements Runnable {
 	}
 
 	public void run() {
+		try {
 		for (int i = this.time; i > -1; i--) {
+				Thread.sleep(1000);
 			System.out.println("Seconds left is: " + i);
+		}
+		} catch (Exception e) {
+			System.out.println(e);
 		}
 	}
 }
