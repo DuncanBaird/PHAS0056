92726c617afcc8b3bed13774d693fe3ca05318da
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-12-03 14:35:36 +0000

    Wrote CountdownTask

--- PHAS0056/src/module8/CountdownTask.java
+++ PHAS0056/src/module8/CountdownTask.java
@@ -1,5 +1,16 @@
 package module8;
 
-public class CountdownTask {
+public class CountdownTask implements Runnable {
 
+	int time;
+
+	public CountdownTask(int time) {
+		this.time = time;
+	}
+
+	public void run() {
+		for (int i = time; i > 0; i--) {
+			System.out.println("Seceonds left is: " + time);
+		}
+	}
 }
