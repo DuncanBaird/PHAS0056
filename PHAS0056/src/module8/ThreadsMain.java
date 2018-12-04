f7f1c2f8f3dee720b53f029059b70afedab56de3
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-12-04 15:41:42 +0000

    ThreadsMain wrote Javadoc

--- PHAS0056/src/module8/ThreadsMain.java
+++ PHAS0056/src/module8/ThreadsMain.java
@@ -2,10 +2,28 @@ package module8;
 
 import java.util.Collections;
 
+/**
+ * Main class for using CountdownTask and PrimeNumberTask. Finds prime numbers
+ * while countdown is running.
+ * 
+ * @author Duncan Baird
+ * @version 1.0
+ */
 public class ThreadsMain {
 
+	/**
+	 * Main method for using CountdownTask and PrimeNumberTask
+	 * <p>
+	 * Starts countdown for given time input in seconds. Then runs the prime checker
+	 * until the countdown stops.
+	 * </p>
+	 * Outputs the largest number checked for being prime, the largest prime number
+	 * found and the total number of prime numbers found.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Implements CountdownTask and PrimeNumebrTask in separate threads
 		Thread countdown = new Thread(new CountdownTask(3));
 		countdown.start();
 
