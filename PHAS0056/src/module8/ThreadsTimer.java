08735d30a26fde8d29b8b2b47fe79cdbdd4c7964
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-12-04 15:19:53 +0000

    Rewrote ThreadTimer to have calculators in methods
    and only call those methods in main method

--- PHAS0056/src/module8/ThreadsTimer.java
+++ PHAS0056/src/module8/ThreadsTimer.java
@@ -8,44 +8,49 @@ import java.util.concurrent.Future;
 
 public class ThreadsTimer {
 
+	public static void singlePi(long points) {
+		double t1 = System.currentTimeMillis();
+		MonteCarloPiCalculatorTask task = new MonteCarloPiCalculatorTask(points);
+		double pi = task.call();
+		System.out.println("The single thread value of pi is: " + pi);
+		System.out.println("The time taken is: " + (System.currentTimeMillis() - t1) + "ms");
+	}
+
+	public static void multiPi(long nPoints, int nThreads) throws Exception {
+		double t2 = System.currentTimeMillis();
+		ExecutorService threadPool = Executors.newFixedThreadPool(nThreads);
+		List<Future<Double>> futures = new ArrayList<Future<Double>>();
+		for (int iThread = 0; iThread < nThreads; ++iThread) {
+			MonteCarloPiCalculatorTask task = new MonteCarloPiCalculatorTask(nPoints / nThreads);
+			Future<Double> future = threadPool.submit(task);
+			futures.add(future);
+		}
+		double sum = 0.0;
+		for (int iThread = 0; iThread < nThreads; ++iThread) {
+			double result = futures.get(iThread).get();
+			sum += result;
+		}
+		threadPool.shutdown();
+		double pi = sum / nThreads;
+		System.out.println("The four thread value of pi is: " + pi);
+		System.out.println("The time taken is: " + (System.currentTimeMillis() - t2) + "ms");
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Calling Pi calculators
 		try {
-			double t1 = System.currentTimeMillis();
-			long nPoints = 10000000L;
-			MonteCarloPiCalculatorTask task = new MonteCarloPiCalculatorTask(nPoints);
-			double pi = task.call();
-			System.out.println("The single thread value of pi is: " + pi);
-			System.out.println("The time taken is: " + (System.currentTimeMillis() - t1) + "ms");
+			singlePi(10000000L);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		try {
+			multiPi(10000000L, 4);
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 
-		try {
-			double t2 = System.currentTimeMillis();
-			long nPoints = 10000000L;
-			int nThreads = 4;
-			ExecutorService threadPool = Executors.newFixedThreadPool(nThreads);
-			List<Future<Double>> futures = new ArrayList<Future<Double>>();
-			for (int iThread = 0; iThread < nThreads; ++iThread) {
-				MonteCarloPiCalculatorTask task = new MonteCarloPiCalculatorTask(nPoints / nThreads);
-				Future<Double> future = threadPool.submit(task);
-				futures.add(future);
-			}
-			double sum = 0.0;
-			for (int iThread = 0; iThread < nThreads; ++iThread) {
-				double result = futures.get(iThread).get();
-				sum += result;
-			}
-			threadPool.shutdown();
-			double pi = sum / nThreads;
-			System.out.println("The four thread value of pi is: " + pi);
-			System.out.println("The time taken is: " + (System.currentTimeMillis() - t2) + "ms");
-
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		System.out.println("The single thread value is more accurate but slower.");
+		System.out.println(
+				"The single thread value is more accurate but slower.\nAnd the time taken is not 4 times faster. ");
 	}
 
 }
