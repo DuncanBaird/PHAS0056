6f1a8bae1257a66b7d2a13e744123bbea6c41cc6
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-12-05 18:25:03 +0000

    Created GUI for manipulating simulation

--- PHAS0056/src/module9/SimulationGUI.java
+++ PHAS0056/src/module9/SimulationGUI.java
@@ -1,5 +1,71 @@
 package module9;
 
-public class SimulationGUI {
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class SimulationGUI extends JPanel implements ActionListener {
+	private static final long serialVersionUID = 1L;
+	private Simulation animPanel; // panel containing animation
+	private JButton startButton;
+	private JButton stopButton;
+	private JButton exitButton;
+
+	/**
+	 * Create JPanel containing animation panel and buttons
+	 */
+	public SimulationGUI() {
+		super();
+		setPreferredSize(new Dimension(600, 600));
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+		animPanel = new Simulation(600, 600);
+		startButton = new JButton("Start");
+		stopButton = new JButton("Stop");
+		exitButton = new JButton("Exit");
+
+		startButton.addActionListener(this);
+		stopButton.addActionListener(this);
+		exitButton.addActionListener(this);
+
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+		buttonPanel.add(startButton);
+		buttonPanel.add(stopButton);
+		buttonPanel.add(exitButton);
+
+		add(animPanel);
+		add(buttonPanel);
+
+	}
+
+	/**
+	 * Start animation when applet is started
+	 */
+	public void start() {
+		animPanel.start();
+	}
+
+	/**
+	 * Stop animation when applet is stopped
+	 */
+	private void stop() {
+		animPanel.stop();
+	}
+
+	/**
+	 * Respond to button clicks
+	 */
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == startButton)
+			start();
+		else if (e.getSource() == stopButton)
+			stop();
+		else if (e.getSource() == exitButton)
+			System.exit(0);
+	}
 }
