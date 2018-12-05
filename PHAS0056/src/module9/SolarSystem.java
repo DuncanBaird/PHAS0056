97ba2e21c6ca6ba18b1c145e9b8484db4ab5c8e1
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-12-05 11:26:40 +0000

    Testing Swing library

--- PHAS0056/src/module9/SolarSystem.java
+++ PHAS0056/src/module9/SolarSystem.java
@@ -1,10 +1,28 @@
 package module9;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
 public class SolarSystem {
+
+	public static void createAndDisplayGUI() {
+		JFrame frame = new JFrame("Swing Example");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel label = new JLabel("Hello");
+		frame.add(label);
+		frame.pack();
+		frame.setVisible(true);
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndDisplayGUI();
+			}
+		});
 	}
 
 }
