72223a07c24513e236b6df8091be9ed13539c397
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-12-10 21:37:11 +0000

    Wrote abstract class StellarObject

--- PHAS0056/src/module9/StellarObject.java
+++ PHAS0056/src/module9/StellarObject.java
@@ -1,5 +1,25 @@
 package module9;
 
-public class StellarObject {
+import java.awt.Color;
 
+public abstract class StellarObject {
+
+	protected String name;
+	protected Position position;
+	protected Position orbitCentre;
+	protected double orbitPeriod;
+	protected Color colour;
+	protected int size;
+
+	protected int size() {
+		return this.size;
+	}
+
+	protected Color colour() {
+		return this.colour;
+	}
+
+	abstract String name();
+
+	abstract Position getPosition();
 }
