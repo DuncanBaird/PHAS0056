f2ce09e377bde8213ce2d3f7375debc8a31b73dc
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-12-10 21:43:09 +0000

    Wrote CircularOrbit, abstract class
    Extends StellarObject

---
+++ PHAS0056/src/module9/CircularOrbit.java
@@ -0,0 +1,11 @@
+package module9;
+
+public abstract class CircularOrbit extends StellarObject {
+
+	protected int orbitRadius;
+	protected double angularSpeed;
+	protected double orbitAngle = 0;
+	protected double delta;
+	protected double delay = 50;
+
+}
