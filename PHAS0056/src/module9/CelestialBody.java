5837c83bdb1967e4c513e6f75058257a07ce1f08
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-12-19 13:36:13 +0000

    Wrote Javadoc for CelestialBody

--- PHAS0056/src/module9/CelestialBody.java
+++ PHAS0056/src/module9/CelestialBody.java
@@ -1,21 +1,37 @@
 package module9;
 
+// import statements
 import java.awt.Color;
 
-/*
- * Class presents an object in the Solar System
+/**
+ * Abstract class for creating celestial objects in solar system
+ * 
+ * @author Duncan Baird
+ * @version 1.0
  */
 public abstract class CelestialBody {
+
+	/**
+	* Member variables for CelestialBody objects
+	*/
 	protected String name;
-	protected Position position; // x,y coordinates of object on animation panel
-	protected Position orbitCentre; // x,y coordinates of orbit centre on animation panel
-	protected double orbitPeriod; // orbit period of object
-	protected Color colour; // colour of object on animation panel
-	protected int size; // size of object on animation panel
+	// (x,y) coordinates in animation
+	protected Position position;
+	// (x,y) coordinates of celestial centre
+	protected Position orbitCentre;
+	protected double orbitPeriod;
+	protected Color colour;
+	protected int size;
+
+	/**
+	* Get methods for size and colour of objects
+	*/
+	protected int size(){return this.size;};
+	protected Color colour(){return this.colour;};
 	
-	protected int size(){return this.size;}; 
-	protected Color colour(){return this.colour;}; 
-	/* Methods to implement in subclasses */
+	/**
+	 * Abstract methods for implementation in subclasses
+	 */
 	abstract String name(); // name of object
 	abstract Position getPosition(); // position of object
 }
