22e803c5172482b65788628979a30cb0ca6b082e
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2018-12-19 13:58:15 +0000

    Wrote Javadoc for Asteroid

--- PHAS0056/src/module9/Asteroid.java
+++ PHAS0056/src/module9/Asteroid.java
@@ -1,30 +1,38 @@
 package module9;
 
+// import statements
 import java.awt.Color;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-/*
- * Class represents an asteroid
+/**
+ * Class for creating asteroids with circular orbits
+ *
+ * @author Duncan Baird
+ * @version 1.0
  */
 public class Asteroid extends CircularOrbitBody {
 
+	/**
+	* Random number used for varying object properties
+	*/
 	private static Random rand = new Random();
 
-	/*
-	 * Construct an asteroid
-	 * 
-	 * @param orbitRadius   radius of orbit
-	 * @param orbitPeriod    orbit period
-	 * @param orbitCentre   centre of orbit
-	 * @param colour        colour of object in animation
-	 * @param size          size of object in animation
+	/**
+	 * Constructs asteroid
+	 *
+	 * @param orbitRadius radius of orbit
+	 * @param orbitPeriod orbital period
+	 * @param orbitCentre centre of orbit
+	 * @param colour      colour of object in animation
+	 * @param size        radius of object in animation
 	 */
 	public Asteroid(int orbitRadius, double orbitPeriod, Position orbitCentre, Color colour, int size) {
+		// inherits from superclass
 		super();
 		this.orbitCentre = orbitCentre;
-		this.orbitRadius = orbitRadius + 
-				(int) (rand.nextGaussian() * 
+		this.orbitRadius = orbitRadius +
+				(int) (rand.nextGaussian() *
 						ThreadLocalRandom.current().nextInt(0, 50 + 1)); // Add Gaussian noise to orbit radius
 		this.orbitPeriod = orbitPeriod * this.orbitRadius/orbitRadius;
 		this.colour = colour;
@@ -35,6 +43,12 @@ public class Asteroid extends CircularOrbitBody {
 		this.orbitAngle = ThreadLocalRandom.current().nextInt(0, 360 + 1); // Random starting angle
 	}
 
+	/**
+	 * Get methods
+	 * 
+	 * @return name name of asteroid
+	 * @return position position of asteroid
+	 */
 	@Override
 	public String name() {
 		return "Asteroid";
@@ -44,7 +58,12 @@ public class Asteroid extends CircularOrbitBody {
 		updatePosition();
 		return this.position;
 	}
-	/* Calculate new position */
+	
+	/**
+	 * Calculate and update asteroid with new position
+	 * <p>
+	 * See planet Javadoc for more specifics
+	 */
 	private void updatePosition() {
 
 		this.orbitAngle += delta;
