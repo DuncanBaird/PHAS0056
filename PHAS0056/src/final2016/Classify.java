043b9809ebac7f45750bac2fd57e404dc63b2ed1
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2019-01-13 14:28:00 +0000

    Created interface Classify

---
+++ PHAS0056/src/final2016/Classify.java
@@ -0,0 +1,12 @@
+package final2016;
+
+/**
+ * Interface for classifying sounds stored as Audio objects
+ * 
+ * @author Duncan
+ *
+ */
+public interface Classify {
+
+	public String classify(Audio a);
+}
