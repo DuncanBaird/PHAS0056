c13fcbf54c3d79dc0f970baf6f952fe1fc3fed69
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2019-01-15 17:10:48 +0000

    Created Flight class with member variables and getters

---
+++ PHAS0056/src/final2017/Flight.java
@@ -0,0 +1,70 @@
+package final2017;
+
+public class Flight {
+
+	private String flightCOde;
+	private String origin;
+	private String destination;
+	private String departureDate;
+	private String departureTime;
+	private String arrivalDate;
+	private String arrivalTime;
+	private double cost;
+
+	/**
+	 * @return the flightCOde
+	 */
+	public String getFlightCOde() {
+		return flightCOde;
+	}
+
+	/**
+	 * @return the origin
+	 */
+	public String getOrigin() {
+		return origin;
+	}
+
+	/**
+	 * @return the destination
+	 */
+	public String getDestination() {
+		return destination;
+	}
+
+	/**
+	 * @return the departureDate
+	 */
+	public String getDepartureDate() {
+		return departureDate;
+	}
+
+	/**
+	 * @return the departureTime
+	 */
+	public String getDepartureTime() {
+		return departureTime;
+	}
+
+	/**
+	 * @return the arrivalDate
+	 */
+	public String getArrivalDate() {
+		return arrivalDate;
+	}
+
+	/**
+	 * @return the arrivalTime
+	 */
+	public String getArrivalTime() {
+		return arrivalTime;
+	}
+
+	/**
+	 * @return the cost
+	 */
+	public double getCost() {
+		return cost;
+	}
+
+}
