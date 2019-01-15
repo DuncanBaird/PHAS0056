8962f0673b4461ce383560e8665de037303bccce
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2019-01-15 20:52:01 +0000

    Created 2 stage system that works

--- PHAS0056/src/final2017/ExamPart3.java
+++ PHAS0056/src/final2017/ExamPart3.java
@@ -1,9 +1,35 @@
 package final2017;
 
+import java.util.ArrayList;
+
 public class ExamPart3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList<Flight> allflights = new ArrayList<Flight>();
+
+		try {
+			allflights = Analysis
+					.flightsFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/exam_data/2017-18/flights.txt");
+
+			FlightFilter f1 = new TwoStageFlights("LHR", "CPT");
+			FlightFilter f2 = new SpecifyAirports("LHR", "CPT");
+
+			ArrayList<Flight> filteredTwoStage = f1.filter(allflights);
+			ArrayList<Flight> filteredOneStage = f2.filter(allflights);
+
+			System.out.println("Flights from LHR to CPT with 2 stages:");
+			for (Flight f : filteredTwoStage) {
+				System.out.println(f);
+			}
+			System.out.println("\nFlights from LHR to CPT that are direct:");
+			for (Flight f : filteredOneStage) {
+				System.out.println(f);
+			}
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 
 	}
 
--- PHAS0056/src/final2017/TwoStageFlights.java
+++ PHAS0056/src/final2017/TwoStageFlights.java
@@ -17,18 +17,30 @@ public class TwoStageFlights implements FlightFilter {
 	public ArrayList<Flight> filter(ArrayList<Flight> list) {
 		// TODO Auto-generated method stub
 		ArrayList<Flight> filtered = new ArrayList<Flight>();
+		try {
+			ArrayList<Airport> airports = new ArrayList<Airport>(
+					Analysis.airportsFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/exam_data/2017-18/airports.txt")
+							.values());
+			for (Airport a : airports) {
+				for (Flight f : list) {
+					if (f.getOrigin().equals(this.origin) && f.getDestination().equals(a.getCode())) {
+						for (Flight j : list) {
+							if (j.getOrigin().equals(a.getCode()) && j.getDestination().equals(this.destination)) {
+								filtered.add(new Flight("2S", f.getOrigin(), j.getDestination(), f.getDepartureDate(),
+										f.getDepartureTime(), j.getArrivalDate(), j.getArrivalTime(), f.getCost()));
+							}
+						}
 
-		for (Flight f : list) {
-			if (f.getOrigin().equals(this.origin) && f.getDestination().equals(this.Stopover)) {
-				for (Flight j : list) {
-					if (j.getOrigin().equals(this.Stopover) && j.getDestination().equals(this.destination)) {
-						filtered.add(f);
-						filtered.add(j);
 					}
 				}
-
 			}
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
 		}
+
+
 		return filtered;
 	}
 
