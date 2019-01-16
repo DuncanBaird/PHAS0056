2883408b082464727458c5fe2d4fa51b3101f018
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2019-01-16 13:53:16 +0000

    Created ExamPart1 and Analysis
    Wrote locationsFromURL in analysis

---
+++ PHAS0056/src/exam2/Analysis.java
@@ -0,0 +1,34 @@
+package exam2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+
+public class Analysis {
+
+	/**
+	 * Returns ArrayList of airports from URL input
+	 * 
+	 * @param url
+	 * @return airports
+	 * @throws IOException
+	 */
+	public static HashMap<Integer, Location> locationsFromURL(String url) throws IOException {
+		HashMap<Integer, Location> locations = new HashMap<Integer, Location>();
+		URL u = new URL(url);
+		InputStream is = u.openStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		String line = "";
+		while ((line = br.readLine()) != null) {
+			Location l = Location.parseLine(line);
+			locations.put(l.getImageUUID(), l);
+		}
+		return locations;
+
+	}
+
+}
