e06aa8dbde0c303aa187193220ca6047504be253
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2019-01-16 13:42:22 +0000

    Locations wrote toString and getter methods

--- PHAS0056/src/exam2/Locations.java
+++ PHAS0056/src/exam2/Locations.java
@@ -16,4 +16,35 @@ public class Locations {
 		this.longitude = longitude;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Locations [imageUUID=" + imageUUID + ", latitude=" + latitude + ", longitude=" + longitude + "]";
+	}
+
+	/**
+	 * @return the imageUUID
+	 */
+	public int getImageUUID() {
+		return imageUUID;
+	}
+
+	/**
+	 * @return the latitude
+	 */
+	public double getLatitude() {
+		return latitude;
+	}
+
+	/**
+	 * @return the longitude
+	 */
+	public double getLongitude() {
+		return longitude;
+	}
+
 }
