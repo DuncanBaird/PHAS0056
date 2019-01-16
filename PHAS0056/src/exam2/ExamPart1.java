7de552cc37459b2a935ab08f9a68221a592e524c
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2019-01-16 13:58:10 +0000

    ExamPart1 pull location data from URL and print result

--- PHAS0056/src/exam2/ExamPart1.java
+++ PHAS0056/src/exam2/ExamPart1.java
@@ -1,10 +1,22 @@
 package exam2;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 public class ExamPart1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		HashMap<Integer, Location> locations = new HashMap<Integer, Location>();
+		try {
+			locations = Analysis
+					.locationsFromURL("http://www.hep.ucl.ac.uk/undergrad/0056/exam-data/2018-19/locations.txt");
+			for (Entry<Integer, Location> entry : locations.entrySet()) {
+				System.out.println(entry);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
 }
