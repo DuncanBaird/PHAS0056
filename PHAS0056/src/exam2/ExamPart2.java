de87a26003f2d0867ace1b153afa3accc7e1bf39
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2019-01-16 15:49:17 +0000

    ExamPart2 selecting species works

---
+++ PHAS0056/src/exam2/ExamPart2.java
@@ -0,0 +1,29 @@
+package exam2;
+
+import java.util.ArrayList;
+
+public class ExamPart2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Location> locations = new ArrayList<Location>();
+
+		try {
+			locations = Analysis
+					.arrayLocationsFromURL("http://www.hep.ucl.ac.uk/undergrad/0056/exam-data/2018-19/locations.txt");
+			ImageSelector s1 = new SpecifySpecies("lion");
+			System.out.println("Starting lion image selection: ");
+			ArrayList<Location> filteredLocations = s1.select(locations);
+			System.out.println("All images identified as lions: ");
+			for (Location l : filteredLocations) {
+				System.out.println(l);
+			}
+
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
+}
