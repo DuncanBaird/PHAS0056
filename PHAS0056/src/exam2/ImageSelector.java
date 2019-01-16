19898c6c8ba5eda22e078f6434fd40411cd3c35b
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2019-01-16 17:08:46 +0000

    Java doc

--- PHAS0056/src/exam2/ImageSelector.java
+++ PHAS0056/src/exam2/ImageSelector.java
@@ -2,6 +2,12 @@ package exam2;
 
 import java.util.ArrayList;
 
+/**
+ * Interface for selecting images as an ArrarList from an ArrayList of images
+ * 
+ * @author
+ *
+ */
 public interface ImageSelector {
 
 	public ArrayList<Location> select(ArrayList<Location> locations) throws Exception;
