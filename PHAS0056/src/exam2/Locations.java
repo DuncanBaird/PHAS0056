5745035392de76600787f7b87720e2a93759ebcb
Duncan Baird <duncan.baird.16@ucl.ac.uk>
2019-01-16 13:41:02 +0000

    Created Locations with member variables and constructors

---
+++ PHAS0056/src/exam2/Locations.java
@@ -0,0 +1,19 @@
+package exam2;
+
+public class Locations {
+
+	private int imageUUID;
+	private double latitude;
+	private double longitude;
+
+	public Locations() {
+		
+	}
+
+	public Locations(int imageUUID, double latitude, double longitude) {
+		this.imageUUID = imageUUID;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+}
